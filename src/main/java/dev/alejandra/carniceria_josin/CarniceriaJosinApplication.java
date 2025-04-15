931e8e0856b9d9f190bfb42389dba7223fcdd44b
Akalchi <alejandrafdez1699@gmail.com>
2025-04-15 10:59:55 +0200

    "new repo"

---
+++ src/main/java/dev/alejandra/carniceria_josin/CarniceriaJosinApplication.java
@@ -0,0 +1,13 @@
+package dev.alejandra.carniceria_josin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarniceriaJosinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarniceriaJosinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alejandra/carniceria_josin/CarniceriaJosinApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alejandra.carniceria_josin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarniceriaJosinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
